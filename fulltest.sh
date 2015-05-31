c371723a3b81a0b269e1bd73f749e883de1a2dc3
Eduardo Pedroni <ep625@york.ac.uk>
2015-05-31 13:38:58 +0200

    Adding a few more tests, travis should work now

--- fulltest.sh
+++ fulltest.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 
-python -m unittest test_shivandragon test_callow test_cardclass test_sorin test_island test_shoal
+python -m unittest test_shivandragon test_callow test_cardclass test_sorin test_island test_shoal test_coecon
